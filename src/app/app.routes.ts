4efb627d1d93441dd1a073aaa398f67b91e260d1
Denis Felst <denis.felst@emilfrey.ch>
2025-12-01 15:01:17 +0100

    [TASK] Initial Angular 19 project setup

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { AppContainerComponent } from './core/layout/app-container.component';
+import { DashboardComponent } from './feature/dashboard/dashboard.component';
+
+export const routes: Routes = [
+  // { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: AppContainerComponent,
+
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      // { path: 'expenses', component: ExpensesComponent },
+      // { path: 'categories', component: CategoriesComponent },
+      // { path: 'settings', component: SettingsComponent },
+      // { path: 'add-expense', component: AddExpenseComponent },
+
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ],
+  },
+
+  { path: '**', redirectTo: '' },
+];
---
+++ src/app/core/layout/app-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-app-container',
+  imports: [RouterOutlet],
+  templateUrl: './app-container.component.html',
+  styleUrl: './app-container.component.scss',
+})
+export class AppContainerComponent {}
