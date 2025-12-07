ede2f7f8e64b8eebf2bd8004043fee0ca66b0b2e
denisfelst <denis-fg@hotmail.com>
2025-12-07 16:50:57 +0100

    [TASK] add settings component

---
+++ src/app/feature/settings/settings.component.ts
@@ -0,0 +1,37 @@
+import { Component, signal } from '@angular/core';
+import { SettingService } from '../../api';
+
+@Component({
+  selector: 'app-settings',
+  standalone: true,
+  imports: [],
+  templateUrl: './settings.component.html',
+  styleUrl: './settings.component.scss',
+})
+export class SettingsComponent {
+  settings = signal<any | null>(null);
+  loading = signal(false);
+  error = signal<string | null>(null);
+
+  constructor(private settingsService: SettingService) {}
+
+  ngOnInit() {
+    this.loadSettings();
+  }
+
+  private loadSettings() {
+    this.loading.set(true);
+    this.error.set(null);
+
+    this.settingsService.getApiSettings(1).subscribe({
+      next: (res) => {
+        this.settings.set(res);
+        this.loading.set(false);
+      },
+      error: () => {
+        this.error.set('Error loading settings');
+        this.loading.set(false);
+      },
+    });
+  }
+}
