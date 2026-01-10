d4c65d72d9285d44b68a7d440041e4b87a26e759
denisfelst <denis-fg@hotmail.com>
2026-01-10 21:14:08 +0100

    [TASK] create environment ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://finament-backend-befbf6c3duhnh4ft.centralus-01.azurewebsites.net',
+};
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { OpenAPI } from './app/core/swagger';
+import { environment } from './environments/environment';
 
-OpenAPI.BASE = process.env['BASE_URL'] ?? 'https://localhost:7001';
+OpenAPI.BASE = environment.baseUrl;
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
