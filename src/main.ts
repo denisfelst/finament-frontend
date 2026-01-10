ec46645bbc4662026324b6d8a94fd765f3fde122
denisfelst <denis-fg@hotmail.com>
2026-01-10 21:06:26 +0100

    [TASK] add env base url

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { OpenAPI } from './app/core/swagger';
 
-OpenAPI.BASE = 'https://localhost:7001';
+OpenAPI.BASE = process.env['BASE_URL'] ?? 'https://localhost:7001';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
