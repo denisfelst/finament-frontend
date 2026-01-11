5a4cc664472b2a5e377cc8be7d25982647de26ed
denisfelst <denis-fg@hotmail.com>
2026-01-11 11:02:10 +0100

    [TASK] add bottom nav component

--- src/app/core/layout/app-container.component.ts
+++ src/app/core/layout/app-container.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
 
 @Component({
   selector: 'app-app-container',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BottomNavComponent],
   templateUrl: './app-container.component.html',
   styleUrl: './app-container.component.scss',
 })
---
+++ src/app/core/layout/bottom-nav/bottom-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-bottom-nav',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './bottom-nav.component.html',
+})
+export class BottomNavComponent {}
